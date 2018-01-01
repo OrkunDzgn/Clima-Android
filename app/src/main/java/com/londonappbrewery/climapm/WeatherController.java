91b0ac04b48f893a6d4794c8fed6d95b64aee1c6
Orkun Düzgün <orkundzgn@gmail.com>
2018-01-01 16:36:22 +0300

    Location permission

--- app/src/main/java/com/londonappbrewery/climapm/WeatherController.java
+++ app/src/main/java/com/londonappbrewery/climapm/WeatherController.java
@@ -1,7 +1,16 @@
 package com.londonappbrewery.climapm;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -19,8 +28,9 @@ public class WeatherController extends AppCompatActivity {
     final float MIN_DISTANCE = 1000;
 
     // TODO: Set LOCATION_PROVIDER here:
+    String LOCATION_PROVIDER = LocationManager.NETWORK_PROVIDER;
 
-
+    final int REQUEST_CODE = 123;
 
 
     // Member Variables:
@@ -29,7 +39,8 @@ public class WeatherController extends AppCompatActivity {
     TextView mTemperatureLabel;
 
     // TODO: Declare a LocationManager and a LocationListener here:
-
+    LocationManager mLocationManager;
+    LocationListener mLocationListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,7 +54,6 @@ public class WeatherController extends AppCompatActivity {
         ImageButton changeCityButton = (ImageButton) findViewById(R.id.changeCityButton);
 
 
-
         // TODO: Add an OnClickListener to the changeCityButton here:
 
     }
@@ -51,15 +61,61 @@ public class WeatherController extends AppCompatActivity {
 
     // TODO: Add onResume() here:
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("Clima", "onResume() Called");
+        getWeatherForCurrentLocation();
+    }
 
 
     // TODO: Add getWeatherForNewCity(String city) here:
 
 
-
     // TODO: Add getWeatherForCurrentLocation() here:
+    private void getWeatherForCurrentLocation() {
+        mLocationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 
+        mLocationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                Log.d("tag", "OnLocationChanged() received");
+            }
 
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String s) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+
+            }
+        };
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.ACCESS_COARSE_LOCATION}, REQUEST_CODE);
+            return;
+        }
+        mLocationManager.requestLocationUpdates(LOCATION_PROVIDER, MIN_TIME, MIN_DISTANCE, mLocationListener);
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Log.d("permission: ", "Permission Granted!");
+                getWeatherForCurrentLocation();
+            }
+        }
+    }
 
     // TODO: Add letsDoSomeNetworking(RequestParams params) here:
 
